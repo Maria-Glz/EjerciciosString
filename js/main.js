51ef3ba2e56c10e585108931a514ebba1a089a4c
maria <maria.gonzalezbarrreda@.com>
2022-12-12 16:11:55 -0600

    Se agregaron ejercicios reverso y palindromo

--- js/main.js
+++ js/main.js
@@ -20,4 +20,34 @@ for (let i=0;i<nombre.length;i++)
    console.log(letra, contfinal);
 
 
+//Entrada 
 
+function reves(nombres)
+{
+    let nombre2 = "";
+    nombre2 = nombre2.toLowerCase();
+
+    for (let i=nombres.length-1; i>=0; i--)
+    {
+        nombre2 += nombres.charAt(i);
+    }
+return (nombre2);
+}
+
+//Salida
+"enilA aruaL"
+
+//Ejercicio 3
+function palin(nombre3)
+{
+    nombre3 = nombre3.toLowerCase();
+    nombre3 = nombre3.replaceAll("","",);
+    if(reves(nombre3)==nombre3)
+    {
+        return true;
+    }
+return false;
+}
+
+//Salida
+"enilA aruaL"
