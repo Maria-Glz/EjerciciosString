d9f731f5c0a4e1667e779419bde5dcc2fcc102a6
maria <maria.gonzalezbarrreda@.com>
2022-12-12 17:20:23 -0600

    Se agrego ejercicios: Validaciones en input

---
+++ ejer2/js/main.js
@@ -0,0 +1,33 @@
+let btnEnviar = document.getElementById("btnEnviar");
+
+btnEnviar.addEventListener("click", function(event){
+  event.preventDefault();
+  let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
+  let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
+  let alertError = document.getElementById("alertError");
+  exampleFormControlTextarea1.value= exampleFormControlTextarea1.value.trim(); 
+  alertError.style.display="none";
+  alertError.innerHTML = "";
+//   console.log(exampleFormControlTextarea1.value.length);
+//   console.log("[" + 
+//          exampleFormControlTextarea1.value.replaceAll("  ", "")
+//          + "]"
+//          );
+
+  if(exampleFormControlTextarea1.value.trim().replaceAll("  ", "").length<20){
+    alertError.innerHTML = "El mensaje debe tener mas de 20 caracteres";
+    alertError.style.display="block";
+    exampleFormControlTextarea1.focus();
+    exampleFormControlTextarea1.select();
+
+  }
+  if (exampleFormControlInput1.value.match(emailRegex)==null)
+  {
+    alertError.style.display="block";
+    alertError.innerHTML += "<br/> El correo electronico no es valido.";
+  }
+
+
+});
